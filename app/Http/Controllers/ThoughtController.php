98d49c37f6d620122f18d740830cf6a2c9fe834d
Tiago Pereira <tiago.pereira.24.1997@gmail.com>
2025-02-11 21:05:44 +0000

    Routes and controllers to update and remove thoughts

--- app/Http/Controllers/ThoughtController.php
+++ app/Http/Controllers/ThoughtController.php
@@ -4,23 +4,74 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Thought; 
+use App\Models\Thought;
+use App\Models\thought as ModelsThought;
+use PhpParser\Node\Expr\FuncCall;
 
 class ThoughtController extends Controller
 {
-    //Show every Thought
+    //Show every thought
     public function index()
     {
-        return view('cloud',['thoughts'=>Thought::latest()->simplePaginate(2)]);
+    return view('cloud',['thoughts'=>Thought::orderby('position','desc')->simplePaginate(6)]);
     }
 
-    //Store the input data
+    //Store a new thought
     public function store(request $request)
     {
         $formsFields = $request->validate([
             'thought'=>'required'
         ]);
-        Thought::create($formsFields);
+
+        $thought=Thought::create($formsFields);
+        $thought->position = $thought->id;
+        $thought->save(); 
         return redirect('/cloud');
+    }
+
+
+    //Make the thought go up or down
+    public function update(Request $request)
+    {
+    
+        $id=$request->route('id');
+        $thoughtClicked=Thought::findOrFail($id);
+        if($request->input('direction')==='up'){
+            //If found a thought above the thought clicked , store the first one on thoughtAbove
+            $thoughtAbove = Thought::where('position','=',$thoughtClicked->position + 1)->first();
+
+            if($thoughtAbove){
+                $thoughtClicked->position++;
+                $thoughtAbove->position--;  
+                $thoughtClicked->save();
+                $thoughtAbove->save();    
+            }
+           
+        }        
+        else if($request->input('direction')==='down'){
+            $thoughtBelow=Thought::where('position','=',$thoughtClicked->position-1)->first();
+
+            if($thoughtBelow){
+                $thoughtClicked->position--;
+                $thoughtBelow->position++;
+                $thoughtClicked->save();
+                $thoughtBelow->save();
+            }
+            
+        };
+
+        return redirect('/cloud');
+    }
+
+
+
+    //Destroy thought
+    public function destroy(Request $request)
+    {
+        $id= $request->route('id');
+        $thought=Thought::findOrFail($id);
+        $thought->delete();
+        return redirect('/cloud');
+       
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\http\Request;    
+
 use App\Http\Controllers\ThoughtController;
-use app\Models\Thought;
+
 Route::get('/cloud', [ThoughtController::class,'index']);
 Route::post('/cloud', [ThoughtController::class,'store']);
+route::patch('/cloud/{id}',[ThoughtController::class,'Update']);
+route::delete('/cloud/{id}',[ThoughtController::class,'destroy']);
